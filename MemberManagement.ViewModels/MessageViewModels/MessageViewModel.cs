cbb49fcd69d7c58ac8384ac32183081ea6e2cfcb
Trần Anh Quốc <trananhquoc22720@gmail.com>
2021-11-25 21:39:52 +0700

    -update

---
+++ MemberManagement.ViewModels/MessageViewModels/MessageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MemberManagement.ViewModels.MessageViewModels
+{
+    public class MessageViewModel
+    {
+        [Required]
+        public string Content { get; set; }
+        public string Timestamp { get; set; }
+        public string From { get; set; }
+        [Required]
+        public string Room { get; set; }
+        public string Avatar { get; set; }
+    }
+}
